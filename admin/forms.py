d290a9bf375757f3f8850928b0726206a15e573a
haojie <holger@haojie.org>
2017-11-12 20:10:41 +0800

    增加 forms 表单

--- admin/forms.py
+++ admin/forms.py
@@ -1,2 +1,15 @@
 # @author HHN
 # @Copyright https://haojie.org
+from flask_wtf import Form
+from wtforms import StringField,FloatField,DateField,RadioField,SelectField
+from wtforms.validators import DataRequired
+
+class EmployeeForm(Form):
+    name =StringField('姓名')
+    gender = RadioField('性别',choices=[('男','男'),('女','女')])
+    job = StringField('职位',default='工程师')
+    department = SelectField('部门')
+    salary = FloatField('工资')
+    birthdate = DateField('生日')
+    idcard = StringField('员工编号')
+    address = StringField('住址')
