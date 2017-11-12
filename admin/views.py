c1a115eb17333f998b61b14a4cf323c613d9ec8d
haojie <holger@haojie.org>
2017-11-12 20:11:32 +0800

    新建 增加员工视图函数

--- admin/views.py
+++ admin/views.py
@@ -1,12 +1,39 @@
 # @author HHN
 # @Copyright https://haojie.org
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,request
 from flask.views import MethodView
-
+from admin.forms import EmployeeForm
+from admin.models import db
 class EmployeeView(MethodView):
+
     def get(self):
         from admin.models import Employee
         emp = Employee.query.limit(20)
         return render_template('emplist.html',emp=emp)
+
     def post(self):
         pass
+
+class AddEmployeeView(MethodView):
+
+    def get(self):
+        from admin.models import Department
+        form = EmployeeForm()
+        form.department.choices=[(d.id,d.name) for d in Department.query.all()]
+        return render_template('addemp.html',form=form)
+
+    def post(self):
+        from admin.models import Employee
+        form = EmployeeForm(request.form)
+        emp = Employee(
+            form.name.data,
+            form.gender.data,
+            form.job.data,
+            form.salary.data,
+            form.birthdate.data,
+            form.idcard.data,
+            form.address.data,
+        )
+        db.session.add(emp)
+        db.commit()
+        return redirect(url_for('admin.emplist'))
