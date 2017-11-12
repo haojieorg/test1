4a2f57b2e62e1312269ff60f5519cb7f08c7a71d
haojie <holger@haojie.org>
2017-11-12 20:09:48 +0800

    把增加员工的视图函数注册到蓝图

--- admin/__init__.py
+++ admin/__init__.py
@@ -4,6 +4,7 @@ from flask import Blueprint
 
 admin_blueprint = Blueprint('admin',__name__)
 
-from .views import EmployeeView
+from .views import EmployeeView,AddEmployeeView
 
 admin_blueprint.add_url_rule('/list/',view_func=EmployeeView.as_view('emplist'))
+admin_blueprint.add_url_rule('/post/',view_func=AddEmployeeView.as_view('addemp'))
