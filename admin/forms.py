5c5ba3b91106f179a1d1eb13bcb50d8b867bbaee
haojie <holger@haojie.org>
2017-11-14 19:31:13 +0800

    department修改为departmentid

--- admin/forms.py
+++ admin/forms.py
@@ -1,14 +1,14 @@
 # @author HHN
 # @Copyright https://haojie.org
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField,FloatField,DateField,RadioField,SelectField
 from wtforms.validators import DataRequired
 
-class EmployeeForm(Form):
-    name =StringField('姓名')
+class EmployeeForm(FlaskForm):
+    name =StringField('姓名',validators=[DataRequired()])
     gender = RadioField('性别',choices=[('男','男'),('女','女')])
     job = StringField('职位',default='工程师')
-    department = SelectField('部门')
+    departmentid = SelectField('部门')
     salary = FloatField('工资')
     birthdate = DateField('生日')
     idcard = StringField('员工编号')
