cae7a26b67dba854003b6585d8276fb39bab4d55
haojieorg <haojie@live.com>
2017-11-16 10:00:25 +0800

    把删除视图添加到蓝图

--- admin/__init__.py
+++ admin/__init__.py
@@ -4,7 +4,7 @@ from flask import Blueprint
 
 admin_blueprint = Blueprint('admin',__name__)
 
-from .views import EmployeeView,AddEmployeeView
+from .views import EmployeeView,AddEmployeeView,EmployeeDeleteView
 
 emplistview=EmployeeView.as_view('emplist')
 
@@ -12,3 +12,6 @@ admin_blueprint.add_url_rule('/list/',view_func=emplistview)
 admin_blueprint.add_url_rule('/list/<int:page>/',view_func=emplistview)
 
 admin_blueprint.add_url_rule('/post/',view_func=AddEmployeeView.as_view('addemp'))
+admin_blueprint.add_url_rule('/edit/<int:id>/',view_func=AddEmployeeView.as_view('edit'))
+
+admin_blueprint.add_url_rule('/del/<int:id>/',view_func=EmployeeDeleteView.as_view('del'))
